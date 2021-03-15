c452d8554c5fbcbadcf82411493c395fb746e77f
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-03-15 16:20:15 -0300

    atualizando o contact form

---
+++ form.js
@@ -0,0 +1,20 @@
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Obrigado pelo contato!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! Ocorreu algum problema durante o envio"
+  });
+}
+form.addEventListener("submit", handleSubmit)
